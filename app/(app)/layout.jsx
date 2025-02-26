1c739585861a5f63c3d6ef58bd056f7903b9a613
MDeFaria <maria.defariasf@gmail.com>
2025-02-26 17:20:38 -0400

    [change] Structure of the main app pages

--- app/(app)/layout.jsx
+++ app/(app)/layout.jsx
@@ -1,5 +1,12 @@
+import Header from "@/components/Header";
 import React from "react";
 
-export default function AppLayout() {
-  return <div>AppLayout</div>;
+export default function AppLayout({ children }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <div className="container mx-auto flex-grow">{children}</div>
+      <footer>footer</footer>
+    </div>
+  );
 }
