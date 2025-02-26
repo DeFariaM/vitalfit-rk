5e0f55a012732153596bcd24529c69d4fc44653a
MDeFaria <maria.defariasf@gmail.com>
2025-02-26 11:20:51 -0400

    [change] initial config of styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,22 @@ const config = {
   ],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#d74545",
+        secondary: "#d6774f",
+        accent: "#475157",
+        bgray: "#475157",
+      },
+      fontFamily: {
+        bebas: "var(--bebas)",
+        montserrat: "var(--montserrat)",
+      },
+      backgroundImage: {
+        pattern: "url('.../public/subtle-prism.svg')",
+        tornado: "url('../public/diamond-sunset.svg')",
+      },
     },
   },
   plugins: ["prettier-plugin-tailwindcss"],
