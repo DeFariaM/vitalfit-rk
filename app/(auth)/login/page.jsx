c302947189a157632899a0f1e3e5c4b635f4db79
MDeFaria <maria.defariasf@gmail.com>
2025-02-26 14:28:46 -0400

    [finish] Login y Signup components

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,5 +1,67 @@
-import React from "react";
+"use client";
+
+import Btn from "@/components/Btn";
+import InputField from "@/components/InputField";
+import { login } from "@/firebase/auth";
+import { handleInputs } from "@/helpers/handleInput";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  return (
+    <div>
+      <h1 className="font-bebas text-bgray text-center text-2xl">
+        ¡Bienvenido de <span className="text-primary">vuelta</span>!
+      </h1>
+      <h2 className="font-bebas text-bgray text-center text-xl">Ingresar</h2>
+      <form className="input-cont">
+        <InputField
+          label={"Email"}
+          name={"email"}
+          type="email"
+          value={user.email}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <InputField
+          label={"Password"}
+          name={"password"}
+          type="password"
+          value={user.password}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <Btn
+          text={"Ingresar"}
+          onClick={async (e) => {
+            e.preventDefault();
+            const { ok } = await login(user);
+            ok && redirect("/");
+          }}
+        />
+      </form>
+      <div className="mt-4">
+        <p className="text-bgray text-center text-sm">
+          <Link
+            className="hover:text-primary underline transition-colors duration-500"
+            href={"/sign-up"}
+          >
+            ¿Olvidaste tu contraseña?{" "}
+          </Link>
+        </p>
+        <p className="text-bgray mt-4 text-center text-sm">
+          ¿Aún no tienes una cuenta?{" "}
+          <Link
+            className="hover:text-primary underline transition-colors duration-500"
+            href={"/sign-up"}
+          >
+            Registrarse
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,5 +1,67 @@
-import React from "react";
+"use client";
+
+import Btn from "@/components/Btn";
+import InputField from "@/components/InputField";
+import { signup } from "@/firebase/auth";
+import { handleInputs } from "@/helpers/handleInput";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { useState } from "react";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  return (
+    <div>
+      <h1 className="font-bebas text-bgray text-center text-2xl">
+        ¡Bienvenido a Vital<span className="text-primary">Fit</span>!
+      </h1>
+      <h2 className="font-bebas text-bgray text-center text-xl">Registrarse</h2>
+      <form className="input-cont">
+        <InputField
+          label={"Nombre"}
+          name={"name"}
+          value={user.name}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <InputField
+          label={"Email"}
+          name={"email"}
+          type="email"
+          value={user.email}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <InputField
+          label={"Password"}
+          name={"password"}
+          type="password"
+          value={user.password}
+          onChange={(e) => handleInputs(e, setUser)}
+        />
+        <Btn
+          text={"Registrarse"}
+          onClick={async (e) => {
+            e.preventDefault();
+            const { ok } = await signup(user);
+            ok && redirect("/");
+          }}
+        />
+      </form>
+      <div className="mt-4">
+        <p className="text-bgray text-center text-sm"></p>
+        <p className="text-bgray mt-4 text-center text-sm">
+          ¿Ya tienes una cuenta?{" "}
+          <Link
+            className="hover:text-primary underline transition-colors duration-500"
+            href={"/login"}
+          >
+            Ingresar
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
