8dff84279541506d8c4691c30f7608f5e9132493
MDeFaria <maria.defariasf@gmail.com>
2025-02-26 13:09:19 -0400

    [add] Component google button for login / signup

---
+++ components/SignBtns.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { signInWithGoogle } from "@/firebase/auth";
+import { FaGoogle } from "react-icons/fa";
+
+export default function SignBtns() {
+  return (
+    <div className="my-2">
+      <button
+        onClick={() => signInWithGoogle()}
+        className="text-bgray hover:bg-bgray mx-auto flex space-x-3 rounded-md px-4 py-2 transition-all duration-500 hover:text-white"
+      >
+        <span>Google</span>
+        <FaGoogle className="h-6 w-6" />
+      </button>
+    </div>
+  );
+}
